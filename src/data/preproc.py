bf06f3aadc2f9c0f73dde17e2ff6b202311ab57b
Mihaela Duta <mihaela.duta@dtc.ox.ac.uk>
2023-12-10 13:27:30 +0000

    Added placeholder pre-processing python code for features.

---
+++ src/data/preproc.py
@@ -0,0 +1,25 @@
+
+def preproc_feature1(dset):
+    ''' Preprocess feature 1
+
+    Args:
+        dset: dataset
+
+    Returns:
+        None
+
+    '''
+    return None
+
+
+def preproc_feature2(dset):
+    ''' Preprocess feature 2
+
+    Args:
+        dset: dataset
+
+    Returns:
+        None
+
+    '''
+    return None
